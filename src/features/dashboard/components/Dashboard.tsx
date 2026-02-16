6ba855642f03c538b04dd3c1fab9593398034a10
cometburn <cometburn@gmail.com>
2026-02-16 20:30:44 +0800

    Edit dashboard

--- src/features/dashboard/components/Dashboard.tsx
+++ src/features/dashboard/components/Dashboard.tsx
@@ -5,26 +5,42 @@ import { useEffect, useState } from "react";
 import { useDashboard } from "@/features/dashboard/hooks/useDashboard";
 import { DashboardRoom } from "../types/dashboard.types";
 import { Button } from "@/components/ui/button";
+import { Socket } from "socket.io-client";
+import { BookingDialog } from "@/features/booking/components/BookingDialog";
 
 export const Dashboard = () => {
   const { user, defaultHotelId, isLoading, error } = useUser();
   const { data: dashboardRooms, isLoading: roomsLoading } = useDashboard();
-  const rooms = dashboardRooms?.data ?? [];
+  // const rooms = dashboardRooms?.data ?? [];
 
-  const [socket, setSocket] = useState(null);
+  const [rooms, setRooms] = useState<DashboardRoom[]>([]);
+  const [_socket, setSocket] = useState<Socket | null>(null);
+
+  // Initialize rooms from API data
+  useEffect(() => {
+    if (dashboardRooms?.data) {
+      setRooms(dashboardRooms.data);
+    }
+  }, [dashboardRooms?.data]);
 
   useEffect(() => {
     const socketInstance = createSocket();
     socketInstance.connect();
 
     socketInstance.on("connect", () => {
-      console.log("Connected to socket");
-      socketInstance.emit("join_hotel", defaultHotelId);
+      socketInstance.emit("join_hotel", `hotel_${defaultHotelId}`);
     });
 
-    socketInstance.on("message", (data) => {
-      console.log("Message received:", data);
+    socketInstance.on('check_in', (data) => {
+      setRooms((prevRooms) =>
+        prevRooms.map((room) =>
+          room.id === data.room_id
+            ? { ...room, bookings: [data] } // Add the new booking
+            : room
+        )
+      );
     });
+
 
     socketInstance.on("connect_error", (error) => {
       console.error("Connection error:", error.message);
@@ -38,7 +54,6 @@ export const Dashboard = () => {
       socketInstance.disconnect();
     };
   }, []);
-
 
   if (isLoading || roomsLoading) {
     return <div className="p-6">Loading dashboard...</div>;
@@ -56,15 +71,24 @@ export const Dashboard = () => {
     <div className="md:max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
       {
         rooms?.map((room: DashboardRoom) => (
-          <Button variant="outline" key={room.id} className="bg-white rounded-md shadow-lg px-2 py-6 md:py-12 text-center">
-            {room.bookings && room.bookings?.length > 0 ? (
-              <span className="text-red-600 text-xl">{room.name}</span>
-            ) : (
-              <span className="text-green-600 text-xl">{room.name}</span>
-            )}
-          </Button>
+          <BookingDialog
+            key={room.id}
+            mode={`${room.bookings && room.bookings?.length > 0 ? "edit" : "add"}`}
+            initialData={room.bookings && room.bookings?.length > 0 ? room.bookings[0] : null}
+            roomData={room}
+            trigger={
+              <Button variant="outline" key={room.id} className="bg-white rounded-md shadow-lg px-2 py-6 md:py-12 text-center">
+                {room.bookings && room.bookings?.length > 0 ? (
+                  <span className="text-red-600 text-xl">{room.name}</span>
+                ) : (
+                  <span className="text-green-600 text-xl">{room.name}</span>
+                )}
+              </Button>
+            }
+          />
         ))
       }
+
     </div >
   );
 };
